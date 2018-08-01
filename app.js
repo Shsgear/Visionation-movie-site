1af696a57a69a632a1757b8089315dc5688240f7
孙皓 <shsgear@qq.com>
2018-08-01 19:54:41 +0800

    changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 	var express = require('express');
 	var path = require('path');
-	1var favicon = require('serve-favicon');
+	var favicon = require('serve-favicon');
 	var logger = require('morgan');
 	var cookieParser = require('cookie-parser');
 	var bodyParser = require('body-parser');
